e71707c74e10f346a00a833068996b8174e42a37
MobolajiJohn <89386125+MobolajiJohn@users.noreply.github.com>
2023-02-02 10:58:56 +0100

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var menuIcon = document.querySelector(".menu-icon");
-var sideBar = document.querySelector(".side-bar");
-var container = document.querySelector(".container");
-
-
-menuIcon.onclick = function() {
-    sideBar.classList.toggle('small-sidebar');
-    container.classList.toggle('large-container');
-}
